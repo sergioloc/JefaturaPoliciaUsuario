fd24750f34f0d226ffc492f5056feb857ba9d7cf
SergioLopezCeballos <sergio.lceballos@alumnos.upm.es>
2018-04-10 18:33:10 +0200

    Registro de email y contraseña

--- app/src/main/java/com/cdi/practica/jefaturapoliciausuario/MainActivity.java
+++ app/src/main/java/com/cdi/practica/jefaturapoliciausuario/MainActivity.java
@@ -1,15 +1,28 @@
 package com.cdi.practica.jefaturapoliciausuario;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView signUp;
+    private TextView signUp;
+    private Button aceptLogin;
+    private EditText email, pass;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,18 +30,76 @@ public class MainActivity extends AppCompatActivity {
 
         init();
         buttons();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user!=null){
+                    //iniciado sesion
+                    //Menu Activity
+
+                }else{
+                    //sesion cerrada
+                }
+            }
+        };
     }
 
     private void init(){
-        //TextView
+        // EditText
+        email = (EditText) findViewById(R.id.email);
+        pass = (EditText) findViewById(R.id.password);
+        // TextView
         signUp = (TextView) findViewById(R.id.signup);
+        // Buttons
+        aceptLogin = (Button) findViewById(R.id.aceptLogin);
     }
+
     private void buttons(){
+        aceptLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailS = email.getText().toString();
+                String passS = pass.getText().toString();
+                if(emailS.equals("")||passS.equals(""))
+                    Toast.makeText(getApplicationContext(),"Debes rellenar ambos campos",Toast.LENGTH_SHORT).show();
+                else
+                    signIn(emailS,passS);
+            }
+        });
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, SignUp.class));
             }
         });
+    }
+
+    private void signIn(String email, String pass){
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),"Sesion Iniciada",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"No se ha podido iniciar sesion",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mAuthListener != null){
+            FirebaseAuth.getInstance().removeAuthStateListener(mAuthListener);
+        }
     }
 }
--- app/src/main/java/com/cdi/practica/jefaturapoliciausuario/SignUp.java
+++ app/src/main/java/com/cdi/practica/jefaturapoliciausuario/SignUp.java
@@ -1,18 +1,27 @@
 package com.cdi.practica.jefaturapoliciausuario;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUp extends AppCompatActivity {
 
-    EditText name, lastname, dni, address, phone;
-    TextView addVehicle, addPropertie, numVehicles, numProperties;
-    Button aceptSignUp;
+    private EditText name, lastname, dni, phone, email, pass, pass2;
+    private TextView addVehicle, addPropertie, numVehicles, numProperties;
+    private Button aceptSignUp;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,18 @@ public class SignUp extends AppCompatActivity {
         init();
         buttons();
 
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user!=null){
+                    //sesion abierta
+                }else{
+                    //sesion cerrada
+                }
+            }
+        };
+
     }
 
     private void init(){
@@ -29,8 +50,10 @@ public class SignUp extends AppCompatActivity {
         name = (EditText) findViewById(R.id.name);
         lastname = (EditText) findViewById(R.id.lastname);
         dni = (EditText) findViewById(R.id.dni);
-        address = (EditText) findViewById(R.id.address);
         phone = (EditText) findViewById(R.id.phone);
+        email = (EditText) findViewById(R.id.email);
+        pass = (EditText) findViewById(R.id.password);
+        pass2 = (EditText) findViewById(R.id.password2);
         // TextView
         addVehicle = (TextView) findViewById(R.id.addVehicle);
         addPropertie = (TextView) findViewById(R.id.addPropertie);
@@ -44,20 +67,66 @@ public class SignUp extends AppCompatActivity {
         aceptSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(SignUp.this, MainActivity.class));
+                String nameS = name.getText().toString();
+                String lastS = lastname.getText().toString();
+                String dniS = dni.getText().toString();
+                String phoneS = phone.getText().toString();
+                String emailS = email.getText().toString();
+                String passS = pass.getText().toString();
+                String pass2S = pass2.getText().toString();
+
+                if(!nameS.equals("") && !lastS.equals("") && !dniS.equals("") && !phoneS.equals("") && !emailS.equals("") && !passS.equals("") && !pass2S.equals("")){
+                    if(passS.equals(pass2S))
+                        if(pass.length()>=6)
+                            signUp(emailS,passS);
+                        else
+                            Toast.makeText(getApplicationContext(),"La contraseña debe tener al menos 6 caracteres",Toast.LENGTH_SHORT).show();
+                    else
+                        Toast.makeText(getApplicationContext(),"La contraseña no coincide",Toast.LENGTH_SHORT).show();
+                }else
+                    Toast.makeText(getApplicationContext(),"Debes rellenar todos los campos",Toast.LENGTH_SHORT).show();
             }
         });
         addVehicle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(SignUp.this, SignUp_Vehicle.class));
+                Toast.makeText(getApplicationContext(),"Añadir vehiculo",Toast.LENGTH_SHORT).show();
+                //startActivity(new Intent(SignUp.this, SignUp_Vehicle.class));
             }
         });
         addPropertie.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(SignUp.this, SignUp_Propertie.class));
+                Toast.makeText(getApplicationContext(),"Añadir propiedad",Toast.LENGTH_SHORT).show();
+                //startActivity(new Intent(SignUp.this, SignUp_Propertie.class));
             }
         });
+    }
+
+    private void signUp(String dni, String pass){
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(dni,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),"Registro completado",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"No se ha podido registrar",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mAuthListener != null){
+            FirebaseAuth.getInstance().removeAuthStateListener(mAuthListener);
+        }
     }
 }
